449df26f523b554bd43ec7a17f0039e4e36d4d97
Alexander Vitchinov <vitalex92@yandex.ru>
2021-09-25 18:21:35 +0300

    Sorting an array (bubble sort)

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,33 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = new int[7];
+            Console.WriteLine("Enter seven Numbers");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("{0} number: ", i + 1);
+                nums[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            int temp;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Display of sorted array");
+            for(int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
         }
     }
 }
