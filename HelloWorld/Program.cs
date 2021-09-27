205f2f12094a8d483440bbeb16cbaf6e34f06acd
Alexander Vitchinov <vitalex92@yandex.ru>
2021-09-27 09:55:52 +0300

    Computing factorial C#;

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,33 +6,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[7];
-            Console.WriteLine("Enter seven Numbers");
-            for (int i = 0; i < nums.Length; i++)
+            static int Factorial(int x)
             {
-                Console.Write("{0} number: ", i + 1);
-                nums[i] = Int32.Parse(Console.ReadLine());
-            }
-
-            int temp;
-
-            for (int i = 0; i < nums.Length - 1; i++)
-            {
-                for (int j = i + 1; j < nums.Length; j++)
+                if (x == 0)
                 {
-                    if (nums[i] > nums[j])
-                    {
-                        temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
-                    }
+                    return 1;
+                }
+                else
+                {
+                    return x * Factorial(x - 1);
                 }
             }
-            Console.WriteLine("Display of sorted array");
-            for(int i = 0; i < nums.Length; i++)
-            {
-                Console.WriteLine(nums[i]);
-            }
+
+            int result = Factorial(5);
+
+            Console.WriteLine(result);
         }
     }
 }
